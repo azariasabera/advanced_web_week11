4adb2a4b211e203aa65e6d8e4372fa1bdf0eb00d
Azarias Galama <129897944+azariasabera@users.noreply.github.com>
2024-07-22 18:03:58 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,11 +20,11 @@ function App() {
             <>
                 <Header />
                 <h1>{t("frontPage")}</h1>
-                <Test1 info={{
+                {/* <Test1 info={{
                     name: 'John Doe',
                     age: 30 }} 
                 />
-                <Test2 name="Jane Doe" age={25} />
+                <Test2 name="Jane Doe" age={25} /> */}
                 <MyContainer />
             </>
           } />
