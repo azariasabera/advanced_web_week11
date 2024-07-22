8c5de818eda7ac473966ee019881009e66dbb30c
Azarias Galama <129897944+azariasabera@users.noreply.github.com>
2024-07-22 16:00:19 +0300

    Done task 2

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+
+const url = 'https://jsonplaceholder.typicode.com/posts';
 
 function About() {
+
+    const [posts, setPosts] = useState([]);
+
+
+    useEffect(() => {
+        let mounted = true; // when the about link is clicked, the component is mounted
+        const fetchData = async () => {
+            const response = await fetch(url);
+            const data = await response.json();
+            if (mounted) {
+                setPosts(data);
+            }
+        }
+        fetchData();
+        return () => mounted = false; // so the component doesn't fetch data after it's unmounted
+    }
+    , []);
   return (
-    <div>About Page</div>
+    <div>
+        About Page
+        <p>{posts.length} posts</p>
+        <ul>
+            {posts.map(post => (
+                <li key={post.id}>{post.title}</li>
+            ))}
+        </ul>
+    </div>
+
   )
 }
 
