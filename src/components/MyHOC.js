8d35175d5a8f09c0c1cc39095e67f7eaec777f5d
Azarias Galama <129897944+azariasabera@users.noreply.github.com>
2024-07-22 23:25:46 +0300

    Update MyHOC.js

--- src/components/MyHOC.js
+++ src/components/MyHOC.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
-// MyHOC is a higher-order component that takes a component and additional props
 const MyHOC = (WrappedComponent, additionalProps) => {
-  // Return a new component that wraps the original component with a div and passes additional props
   return (props) => {
     return (
       <div className="wrapper">
